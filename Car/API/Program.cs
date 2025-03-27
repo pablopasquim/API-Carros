121f3fa472e8cf79c2af24b402b9d1534d85f092
PABLO DA SILVA PASQUIM <pablo.pasquim@cs.up.edu.br>
2025-03-27 11:29:23 -0300

    Feat: Created EndPoints = Get,Post,Put and Delete for BMW cars

--- Car/API/Program.cs
+++ Car/API/Program.cs
@@ -2,19 +2,72 @@ using API.Models;
 using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<AppDataContext>();
 var app = builder.Build();
 
-List<Carro> carros = new List<Carro>() {
-    new Carro { Id = 1, Name = "Fusca" },
-    new Carro { Id = 2, Name = "Ferrari" }
-};
+// Endpoints = relacionados ao recursos de Carros
 
-// Exemplo requisição
-app.MapGet("/", () => "Hello World!");
+// GET: api/carros = Lista de todos os carros cadastrados
+app.MapGet("/api/carros", 
+    ([FromServices] AppDataContext ctx) => {
 
-// GET: api/carros
-app.MapGet("/api/carros", () => {
-    return Results.Ok(carros);
+    if(ctx.Carros.Any()){
+        return Results.Ok(ctx.Carros.ToList());
+    }
+    return Results.NotFound();
+});
+
+// POST: api/carros/cadastrar = Cadastrar carros e salvar
+app.MapPost("/api/carros/cadastrar", 
+    ([FromBody] Carro carro, [FromServices] AppDataContext ctx) => {
+
+    ctx.Carros.Add(carro);
+    ctx.SaveChanges();
+    return Results.Created("", carro);
+});
+
+// GET: Buscar carro pelo ID
+app.MapGet("/api/carros/{id}", 
+    ([FromRoute] int Id, [FromServices] AppDataContext ctx) => {
+
+    Carro? carro = ctx.Carros.Find(Id);
+
+    if(carro != null){
+        return Results.Ok(carro);
+    }
+
+    return Results.NotFound();
+});
+
+// PUT: Atualiza os dados de um carro pelo ID
+app.MapPut("/api/carros/{id}", 
+    ([FromRoute] int Id, [FromBody] Carro carro, [FromServices] AppDataContext ctx) => {
+
+    Carro? entidade = ctx.Carros.Find(Id);
+
+    if(entidade != null){
+        entidade.Name = carro.Name;
+        ctx.Carros.Update(entidade);
+        ctx.SaveChanges();
+        return Results.Ok(entidade);
+    }
+
+    return Results.NotFound();
+});
+
+// DELETE: Remover um carro pelo ID
+app.MapDelete("/api/carros/{id}", 
+    ([FromRoute] int Id, [FromServices] AppDataContext ctx) => {
+    
+    Carro? carro = ctx.Carros.Find(Id);
+
+    if(carro == null){
+        return Results.NotFound();
+    }
+
+    ctx.Carros.Remove(carro);
+    ctx.SaveChanges();
+    return Results.NoContent();
 });
 
 app.Run();
